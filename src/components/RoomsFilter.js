a3b8ea11e4e50d821d75547fc7f41e4492a0104c
kristou799@gmail.com <kristiyan.svetoslavov.petkov@gmail.com>
2021-10-08 13:54:50 +0300

    added rooms list and rooms filter first input field

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "./Title";
 
-const RoomsFilter = () => {
-  return <div>zdr</div>;
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+const RoomsFilter = ({ rooms }) => {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  console.log(type);
+
+  let types = getUnique(rooms, "type");
+  types = ["all", ...types];
+  types = types.map((type, i) => {
+    return (
+      <option value={type} key={i}>
+        {type}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default RoomsFilter;
--- src/context.js
+++ src/context.js
@@ -4,22 +4,41 @@ import items from "./data";
 const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
-  state = {
-    rooms: [],
-    sortedRooms: [],
-    featuredRooms: [],
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: [],
+      sortedRooms: [],
+      featuredRooms: [],
+      loading: true,
+      type: "all",
+      capacity: 1,
+      price: 0,
+      minPrice: 0,
+      maxPrice: 0,
+      minSize: 0,
+      maxSize: 0,
+      breakfast: false,
+      pets: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
 
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
-
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -45,9 +64,41 @@ export default class RoomProvider extends Component {
     return room;
   };
 
+  handleChange(e) {
+    const target = e.target;
+    const value = e.type === "checkbox" ? target.checked : target.value;
+    const name = e.target.name;
+
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterRooms
+    );
+  }
+
+  filterRooms() {
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter((room) => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    });
+  }
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -55,5 +106,15 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function WrappedComponent(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
 
 export { RoomConsumer, RoomProvider, RoomContext };
